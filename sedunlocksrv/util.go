f58865eda0d6e325066a2b185bffaea224e4eb3c
scoutdriver73 <scoutdriver73@users.noreply.github.com>
2026-03-27 22:27:30 -0700

    Remove verbose flag from sedutil command in runExpertPBAFlashBytes function

--- sedunlocksrv/util.go
+++ sedunlocksrv/util.go
@@ -190,9 +190,9 @@ func runExpertPBAFlashBytes(w http.ResponseWriter, password string, imageData []
 	// the firmware recover between operations.
 	time.Sleep(250 * time.Millisecond)
 
-	out, err := runSedutil(2*time.Minute, "-vvv", "--loadpbaimage", password, tmpPath, device)
+	out, err := runSedutil(2*time.Minute, "--loadpbaimage", password, tmpPath, device)
 	resp := map[string]string{
-		"command": "sedutil-cli -vvv --loadpbaimage <password> <uploaded-image> " + device,
+		"command": "sedutil-cli --loadpbaimage <password> <uploaded-image> " + device,
 		"output":  strings.TrimSpace(out),
 	}
 	respAny := map[string]interface{}{
